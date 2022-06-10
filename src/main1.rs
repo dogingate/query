2a6d15ef6337d16ce06e9fb1c3c29c2e8f5149f6
vibration007 <dogingate@qq.com>
2022-06-10 13:49:12 +0800

    work

---
+++ src/main1.rs
@@ -0,0 +1,61 @@
+/*
+ * @Author: vibraion007 dogingate@qq.com
+ * @Date: 2022-05-02 22:14:06
+ * @LastEditors: vibration007 dogingate@qq.com
+ * @LastEditTime: 2022-06-10 13:46:15
+ * @FilePath: \rust\hello\src\main.rs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @Author: vibraion007 dogingate@qq.com
+ * @Date: 2022-05-02 22:14:06
+ * @LastEditors: vibraion007 dogingate@qq.com
+ * @LastEditTime: 2022-05-04 11:17:14
+ * @FilePath: \rust\hello\src\main.rs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// mod types;
+// mod strings;
+// mod tuples;
+// mod arrays;
+// mod vectors;
+// mod conditions;
+// mod loops;
+// mod functions;
+// mod pointers;
+// mod structures;
+mod query;
+// mod rectangle;
+fn main() {
+    query::main();
+    // rectangle::run();
+}
+// let mut width = String::new();
+// io::stdin().read_line(&mut width).expect("Error reading");
+// let width: f32 = match width.trim().parse() {
+//     Ok(num) => num,
+//     Err(_) => continue,
+// };
+// match guess.cmp(&secret_num) {
+//     Ordering::Less => println!("Tool small"),
+//     Ordering::Greater => println!("Too big"),
+//     Ordering::Equal => {
+//         println!("You win!");
+//         break;
+//     }
+// }
+
+// let number = 7;
+// if direction  == 1 {
+//     println!("yes ");
+// } else if number % 3 == 0 {
+//     println!("no ");
+// } else {
+//     println!("no no ");
+// }
+
+//     let condition = true;
+//     let number2 = if condition { 1 } else { 0 };
+//     println!("the value of number is :{}", number2);
+// }
+// }
